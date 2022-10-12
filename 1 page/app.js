f6aadb9e6424e9a53e863fd8c581959f69c2f15a
sergei <sverkeenko1303@gmail.com>
2022-10-12 20:34:59 +0300

    correction

--- 1 page/app.js
+++ 1 page/app.js
@@ -5,7 +5,6 @@ function checkEnter(inputEmail, inputPassword) {
     try {
         if (!/^\+[0-9]{12}|^[\w\.\_]+@[a-z]+\.[a-z]{2,4}$/g.test(inputEmail)) throw new Error('Не корректный e-mail')
         if (!/[0-9]/g.test(inputPassword)) throw new Error('Не корректный пароль')
-        return alert('Вы успешно зарегистрированы')
 
     } catch (error) {
         return alert(error.message)
@@ -16,6 +15,9 @@ btn.addEventListener('click', () => {
     const inputEmail = document.querySelector('.input-email').value
     const inputPassword = document.querySelector('.input-password').value
     checkEnter(inputEmail, inputPassword)
+    document.querySelector('.input-email').value = '';
+    document.querySelector('.input-password').value = ''
+    return alert('Вы успешно зарегистрированы')
 })
 
 btn.addEventListener('mouseover', () => {
--- 2 page/app2.js
+++ 2 page/app2.js
@@ -4,7 +4,6 @@ function checkEnter(inputEmail, inputPassword, inputPasswordCheck) {
     try {
         if (!/^\+[0-9]{12}|^[\w\.\_]+@[a-z]+\.[a-z]{2,4}$/g.test(inputEmail)) throw new Error('Не корректный e-mail')
         if (!(inputPasswordCheck == inputPassword)) throw new Error('Пароли не совпадают')
-        return alert('Вы успешно зарегистрированы')
 
     } catch (error) {
         return alert(error.message)
@@ -16,6 +15,10 @@ btn.addEventListener('click', () => {
     const inputEmail = document.querySelector('.input-email').value
     const inputPassword = document.querySelector('.input-password').value
     checkEnter(inputEmail, inputPassword, inputPasswordCheck)
+    document.querySelector('.input-password-check').value = ''
+    document.querySelector('.input-email').value = ''
+    document.querySelector('.input-password').value = ''
+    return alert('Вы успешно зарегистрированы')
 })
 
 btn.addEventListener('mouseover', () => {
